bed03a640699b9d7b0ac44db3257159648426602
Qusai Farraj <qusai919@gmail.com>
2018-03-31 21:12:13 -0500

    added more views. updated extension to twig.

--- app/views/email/auth/password/recover.php
+++
@@ -1,7 +0,0 @@
-{% extends 'email/templates/default.php' %}
-
-{% block content %}
-    <p>You requseted a password change.</p>
-
-    <p>Click this link to reset your password: {{ baseUrl }}{{ urlFor('password.reset') }}?email={{ user.email }}&identifier={{ identifier|url_encode }}</p>
-{% endblock %}
--- app/views/email/auth/registered.php
+++
@@ -1,7 +0,0 @@
-{% extends 'email/templates/default.php' %}
-
-{% block content %}
-    <p>You are registered!</p>
-
-    <p>Ativate your account using this link: {{ baseUrl }}{{ urlFor('activate') }}?email={{ user.email }}&identifier={{ identifier|url_encode }}</p>
-{% endblock %}
